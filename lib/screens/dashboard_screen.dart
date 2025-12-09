06ab78fd1834ee969e65fdf3b181ffb0b905f348
vinayakstha <vsth005@gmail.com>
2025-12-09 21:00:46 +0545

    bottom navigation added

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:ghar_care/screens/bottom_screens/favourite_screen.dart';
+import 'package:ghar_care/screens/bottom_screens/home_screen.dart';
+import 'package:ghar_care/screens/bottom_screens/profile_screen.dart';
+import 'package:ghar_care/screens/bottom_screens/task_screen.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -8,8 +12,31 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
+
+  List<Widget> lstBottomScreen = [
+    const HomeScreen(),
+    const TaskScreen(),
+    const FavouriteScreen(),
+    const ProfileScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Dashboard")));
+    return Scaffold(
+      appBar: AppBar(title: Text("Dashboard")),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: "Task",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "fav"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
   }
 }
