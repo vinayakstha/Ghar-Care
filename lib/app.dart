b3b8ce1b2b843b715c13fb26e23867634e67df36
vinayakstha <vsth005@gmail.com>
2025-12-15 20:05:44 +0545

    theme-data

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:ghar_care/screens/splash_screen.dart';
+import 'package:ghar_care/theme/theme_data.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: getApplicationTheme(),
+      home: SplashScreen(),
+    );
   }
 }
---
+++ lib/theme/theme_data.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(fontFamily: "Inter Regular");
+}
