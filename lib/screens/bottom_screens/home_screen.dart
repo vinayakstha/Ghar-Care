deaa1cbafc820dda4b948cb6c0ae0de7e4f6361a
vinayakstha <vsth005@gmail.com>
2025-12-17 13:26:30 +0545

    offer card added in list view

--- lib/screens/bottom_screens/home_screen.dart
+++ lib/screens/bottom_screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ghar_care/widgets/home_header.dart';
+import 'package:ghar_care/widgets/offer_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Widget> promo = [
+    OfferCard(
+      title: "20% OFF",
+      description1: "For 1 day",
+      description2: "D3T22U",
+      imagePath: "assets/images/offer_image.jpg",
+    ),
+    OfferCard(
+      title: "20% OFF",
+      description1: "For 1 day",
+      description2: "D3T22U",
+      imagePath: "assets/images/offer_image.jpg",
+    ),
+    OfferCard(
+      title: "20% OFF",
+      description1: "For 1 day",
+      description2: "D3T22U",
+      imagePath: "assets/images/card_image.png",
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
@@ -16,7 +38,46 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           HomeHeader(),
 
-          Expanded(child: SingleChildScrollView(child: SizedBox(height: 1000))),
+          Expanded(
+            child: SingleChildScrollView(
+              child: SizedBox(
+                // height: 1000,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 190,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: promo.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(right: 12),
+                            child: promo[index],
+                          );
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Categories", style: TextStyle(fontSize: 22)),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "View More",
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/widgets/offer_card.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class OfferCard extends StatelessWidget {
+  const OfferCard({
+    super.key,
+    required this.title,
+    required this.description1,
+    required this.description2,
+    required this.imagePath,
+  });
+
+  final String title;
+  final String description1;
+  final String description2;
+  final String imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8, 9, 0, 1),
+      child: Container(
+        height: 160,
+        width: MediaQuery.of(context).size.width * 0.9,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(16)),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(16),
+          child: Stack(
+            children: [
+              Positioned.fill(child: Image.asset(imagePath, fit: BoxFit.cover)),
+
+              Positioned.fill(
+                child: Container(color: Colors.black.withOpacity(0.3)),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 26,
+                        fontFamily: "Inter Bold",
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      description1,
+                      style: const TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                    const SizedBox(height: 8),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.card_giftcard,
+                          color: Colors.white,
+                          size: 18,
+                        ),
+                        const SizedBox(width: 6),
+                        Text(
+                          description2,
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
