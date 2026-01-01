1a284ec9c26a626c732ca8e61bc608c3644ccbdc
vinayakstha <vsth005@gmail.com>
2026-01-01 19:31:34 +0545

    data layer repo done

---
+++ lib/features/auth/data/repositories/auth_repository.dart
@@ -0,0 +1,76 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ghar_care/core/error/failures.dart';
+import 'package:ghar_care/features/auth/data/datasource/auth_datasource.dart';
+import 'package:ghar_care/features/auth/data/datasource/local/auth_local_datasource.dart';
+import 'package:ghar_care/features/auth/data/models/auth_hive_model.dart';
+import 'package:ghar_care/features/auth/domain/entities/auth_entity.dart';
+import 'package:ghar_care/features/auth/domain/repositories/auth_repository.dart';
+
+//provider
+final authRepository = Provider<IAuthRepository>((ref) {
+  return AuthRepository(authDatasource: ref.read(AuthLocalDatasourceProvider));
+});
+
+class AuthRepository implements IAuthRepository {
+  final IAuthDatasource _authDatasource;
+
+  AuthRepository({required IAuthDatasource authDatasource})
+    : _authDatasource = authDatasource;
+
+  @override
+  Future<Either<Failure, AuthEntity>> getCurrentUser() async {
+    try {
+      final user = await _authDatasource.getCurrentUser();
+      if (user != null) {
+        final entity = user.toEntity();
+        return Right(entity);
+      }
+    } catch (e) {
+      return Left(LocalDatabaseFailure(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, AuthEntity>> login(
+    String email,
+    String password,
+  ) async {
+    try {
+      final user = await _authDatasource.login(email, password);
+      if (user != null) {
+        final entity = user.toEntity();
+        return Right(entity);
+      }
+    } catch (e) {
+      return Left(LocalDatabaseFailure(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> logout() async {
+    try {
+      final result = await _authDatasource.logoutUser();
+      if (result) {
+        return Right(true);
+      }
+      return Left(LocalDatabaseFailure(message: "failed to logout user"));
+    } catch (e) {
+      return Left(LocalDatabaseFailure(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> register(AuthEntity authEntity) async {
+    try {
+      final model = AuthHiveModel.fromEntity(authEntity);
+      final result = await _authDatasource.register(model);
+      if (result) {
+        return Right(true);
+      }
+      return Left(LocalDatabaseFailure(message: "failed to register user"));
+    } catch (e) {
+      return Left(LocalDatabaseFailure(message: e.toString()));
+    }
+  }
+}
