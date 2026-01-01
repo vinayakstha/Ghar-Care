e84fc6d3f36f2efb758a16b4e683c552a6d3d189
vinayakstha <vsth005@gmail.com>
2026-01-01 15:40:55 +0545

    hive service

---
+++ lib/core/services/hive/hive_service.dart
@@ -0,0 +1,59 @@
+import 'package:ghar_care/core/constants/hive_table_constant.dart';
+import 'package:ghar_care/features/auth/data/models/auth_hive_model.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+class HiveService {
+  //init
+  Future<void> init() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = '${directory.path}/${HiveTableConstant.dbName}';
+    Hive.init(path);
+    _registerAdapter();
+  }
+
+  //register adapter
+  void _registerAdapter() {
+    if (Hive.isAdapterRegistered(HiveTableConstant.authTypeId)) {
+      Hive.registerAdapter(AuthHiveModelAdapter());
+    }
+  }
+
+  //open boxes
+  Future<void> openBoxes() async {
+    await Hive.openBox<AuthHiveModel>(HiveTableConstant.authTable);
+  }
+
+  //close boxes
+  Future<void> closeBoxes() async {
+    await Hive.close();
+  }
+
+  //queries
+  //============== AUTH QUERIES ================
+  Box<AuthHiveModel> get _authBox =>
+      Hive.box<AuthHiveModel>(HiveTableConstant.authTable);
+
+  Future<AuthHiveModel> registerUser(AuthHiveModel model) async {
+    await _authBox.put(model.authId, model);
+    return model;
+  }
+
+  Future<AuthHiveModel?> loginUser(String email, String password) async {
+    final users = _authBox.values.where(
+      (user) => user.email == email && user.password == password,
+    );
+    if (users.isNotEmpty) {
+      return users.first;
+    }
+    return null;
+  }
+
+  Future<void> logout(String authId) async {
+    await _authBox.delete(authId);
+  }
+
+  AuthHiveModel? getCurrentUser(String authId) {
+    return _authBox.get(authId);
+  }
+}
